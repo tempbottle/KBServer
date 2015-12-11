71286c408e406b648b0a6b47b40d049728198b52
ChineseFish <565828928@qq.com>
2015-12-11 10:44:02 +0800

    init

---
+++ kbengine-master/kbe/src/server/tools/interfaces/interfaces.cpp
@@ -0,0 +1,348 @@
+/*
+This source file is part of KBEngine
+For the latest info, see http://www.kbengine.org/
+
+Copyright (c) 2008-2016 KBEngine.
+
+KBEngine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+KBEngine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with KBEngine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "orders.h"
+#include "interfaces.h"
+#include "interfaces_tasks.h"
+#include "anonymous_channel.h"
+#include "interfaces_interface.h"
+#include "network/common.h"
+#include "network/tcp_packet.h"
+#include "network/udp_packet.h"
+#include "network/message_handler.h"
+#include "thread/threadpool.h"
+#include "server/components.h"
+
+
+#include "baseapp/baseapp_interface.h"
+#include "cellapp/cellapp_interface.h"
+#include "baseappmgr/baseappmgr_interface.h"
+#include "cellappmgr/cellappmgr_interface.h"
+#include "loginapp/loginapp_interface.h"
+#include "dbmgr/dbmgr_interface.h"
+
+namespace KBEngine{
+	
+ServerConfig g_serverConfig;
+KBE_SINGLETON_INIT(Interfaces);
+
+//-------------------------------------------------------------------------------------
+Interfaces::Interfaces(Network::EventDispatcher& dispatcher, 
+			 Network::NetworkInterface& ninterface, 
+			 COMPONENT_TYPE componentType,
+			 COMPONENT_ID componentID):
+	ServerApp(dispatcher, ninterface, componentType, componentID),
+	mainProcessTimer_(),
+	reqCreateAccount_requests_(),
+	reqAccountLogin_requests_(),
+	mutex_()
+{
+}
+
+//-------------------------------------------------------------------------------------
+Interfaces::~Interfaces()
+{
+	mainProcessTimer_.cancel();
+	lockthread();
+
+	if(reqCreateAccount_requests_.size() > 0)
+	{	
+		int i = 0;
+		REQCREATE_MAP::iterator iter = reqCreateAccount_requests_.begin();
+		for(; iter != reqCreateAccount_requests_.end(); ++iter)
+		{
+			WARNING_MSG(fmt::format("Interfaces::~Interfaces(): Discarding {0}/{1} reqCreateAccount[{2:p}] tasks.\n", 
+				++i, reqCreateAccount_requests_.size(), (void*)iter->second));
+		}
+	}
+
+	if(reqAccountLogin_requests_.size() > 0)
+	{
+		int i = 0;
+		REQLOGIN_MAP::iterator iter = reqAccountLogin_requests_.begin();
+		for(; iter != reqAccountLogin_requests_.end(); ++iter)
+		{
+			WARNING_MSG(fmt::format("Interfaces::~Interfaces(): Discarding {0}/{1} reqAccountLogin[{2:p}] tasks.\n", 
+				++i, reqAccountLogin_requests_.size(), (void*)iter->second));
+		}
+	}
+
+	unlockthread();
+}
+
+//-------------------------------------------------------------------------------------	
+void Interfaces::onShutdownEnd()
+{
+	ServerApp::onShutdownEnd();
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::lockthread()
+{
+	mutex_.lockMutex();
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::unlockthread()
+{
+	mutex_.unlockMutex();
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::eraseOrders_s(std::string ordersid)
+{
+	lockthread();
+
+	ORDERS::iterator iter = orders_.find(ordersid);
+	if(iter != orders_.end())
+	{
+		ERROR_MSG(fmt::format("Interfaces::eraseOrders_s: chargeID={} not found!\n", ordersid));
+	}
+
+	orders_.erase(iter);
+	unlockthread();
+}
+
+//-------------------------------------------------------------------------------------
+bool Interfaces::hasOrders(std::string ordersid)
+{
+	bool ret = false;
+	
+	lockthread();
+	ORDERS::iterator iter = orders_.find(ordersid);
+	ret = (iter != orders_.end());
+	unlockthread();
+	
+	return ret;
+}
+
+//-------------------------------------------------------------------------------------
+bool Interfaces::run()
+{
+	return ServerApp::run();
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::handleTimeout(TimerHandle handle, void * arg)
+{
+	switch (reinterpret_cast<uintptr>(arg))
+	{
+		case TIMEOUT_TICK:
+			this->handleMainTick();
+			break;
+		default:
+			break;
+	}
+
+	ServerApp::handleTimeout(handle, arg);
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::handleMainTick()
+{
+	 //time_t t = ::time(NULL);
+	 //DEBUG_MSG("Interfaces::handleGameTick[%"PRTime"]:%u\n", t, time_);
+	
+	g_kbetime++;
+	threadPool_.onMainThreadTick();
+	networkInterface().processChannels(&InterfacesInterface::messageHandlers);
+}
+
+//-------------------------------------------------------------------------------------
+bool Interfaces::initializeBegin()
+{
+	return true;
+}
+
+//-------------------------------------------------------------------------------------
+bool Interfaces::inInitialize()
+{
+	// 广播自己的地址给网上上的所有kbemachine
+	Components::getSingleton().pHandler(this);
+	return true;
+}
+
+//-------------------------------------------------------------------------------------
+bool Interfaces::initializeEnd()
+{
+	mainProcessTimer_ = this->dispatcher().addTimer(1000000 / g_kbeSrvConfig.gameUpdateHertz(), this,
+							reinterpret_cast<void *>(TIMEOUT_TICK));
+
+	// 不做频道超时检查
+	CLOSE_CHANNEL_INACTIVITIY_DETECTION();
+
+	this->threadPool().addTask(new AnonymousChannel());
+
+	return initDB();
+}
+
+//-------------------------------------------------------------------------------------		
+bool Interfaces::initDB()
+{
+	return true;
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::finalise()
+{
+	ServerApp::finalise();
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::reqCreateAccount(Network::Channel* pChannel, KBEngine::MemoryStream& s)
+{
+	std::string registerName, accountName, password, datas;
+	COMPONENT_ID cid;
+	uint8 accountType = 0;
+
+	s >> cid >> registerName >> password >> accountType;
+	s.readBlob(datas);
+	
+	if(accountType == (uint8)ACCOUNT_TYPE_MAIL)
+	{
+	}
+
+	lockthread();
+
+	REQCREATE_MAP::iterator iter = reqCreateAccount_requests_.find(registerName);
+	if(iter != reqCreateAccount_requests_.end())
+	{
+		unlockthread();
+		return;
+	}
+
+	CreateAccountTask* pinfo = new CreateAccountTask();
+	pinfo->commitName = registerName;
+	pinfo->accountName = registerName;
+	pinfo->getDatas = "";
+	pinfo->password = password;
+	pinfo->postDatas = datas;
+	pinfo->retcode = SERVER_ERR_OP_FAILED;
+	pinfo->baseappID = cid;
+	pinfo->dbmgrID = pChannel->componentID();
+	pinfo->address = pChannel->addr();
+	pinfo->enable = true;
+
+	reqCreateAccount_requests_[pinfo->commitName] = pinfo;
+	unlockthread();
+
+	this->threadPool().addTask(pinfo);
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::onAccountLogin(Network::Channel* pChannel, KBEngine::MemoryStream& s) 
+{
+	std::string loginName, accountName, password, datas;
+	COMPONENT_ID cid;
+
+	s >> cid >> loginName >> password;
+	s.readBlob(datas);
+
+	lockthread();
+
+	REQLOGIN_MAP::iterator iter = reqAccountLogin_requests_.find(loginName);
+	if(iter != reqAccountLogin_requests_.end())
+	{
+		unlockthread();
+		return;
+	}
+
+	LoginAccountTask* pinfo = new LoginAccountTask();
+	pinfo->commitName = loginName;
+	pinfo->accountName = loginName;
+	pinfo->getDatas = "";
+	pinfo->password = password;
+	pinfo->postDatas = datas;
+	pinfo->retcode = SERVER_ERR_OP_FAILED;
+	pinfo->baseappID = cid;
+	pinfo->dbmgrID = pChannel->componentID();
+	pinfo->address = pChannel->addr();
+	pinfo->enable = true;
+
+	reqAccountLogin_requests_[pinfo->commitName] = pinfo;
+	unlockthread();
+
+	this->threadPool().addTask(pinfo);
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::charge(Network::Channel* pChannel, KBEngine::MemoryStream& s)
+{
+	OrdersCharge* pOrdersCharge = new OrdersCharge();
+
+	pOrdersCharge->timeout = timestamp()  + uint64(g_kbeSrvConfig.interfaces_orders_timeout_ * stampsPerSecond());
+
+	pOrdersCharge->dbmgrID = pChannel->componentID();
+	pOrdersCharge->address = pChannel->addr();
+
+	s >> pOrdersCharge->baseappID;
+	s >> pOrdersCharge->ordersID;
+	s >> pOrdersCharge->dbid;
+	s.readBlob(pOrdersCharge->postDatas);
+	s >> pOrdersCharge->cbid;
+
+	INFO_MSG(fmt::format("Interfaces::charge: componentID={4}, chargeID={0}, dbid={1}, cbid={2}, datas={3}!\n",
+		pOrdersCharge->ordersID, pOrdersCharge->dbid, pOrdersCharge->cbid, pOrdersCharge->postDatas, pOrdersCharge->baseappID));
+
+	lockthread();
+
+	ORDERS::iterator iter = orders_.find(pOrdersCharge->ordersID);
+	if(iter != orders_.end())
+	{
+		ERROR_MSG(fmt::format("Interfaces::charge: chargeID={} is exist!\n", pOrdersCharge->ordersID));
+		delete pOrdersCharge;
+		unlockthread();
+		return;
+	}
+
+	ChargeTask* pinfo = new ChargeTask();
+	pinfo->orders = *pOrdersCharge;
+	pinfo->pOrders = pOrdersCharge;
+	orders_[pOrdersCharge->ordersID].reset(pOrdersCharge);
+	unlockthread();
+	
+	this->threadPool().addTask(pinfo);
+}
+
+//-------------------------------------------------------------------------------------
+void Interfaces::eraseClientReq(Network::Channel* pChannel, std::string& logkey)
+{
+	lockthread();
+
+	REQCREATE_MAP::iterator citer = reqCreateAccount_requests_.find(logkey);
+	if(citer != reqCreateAccount_requests_.end())
+	{
+		citer->second->enable = false;
+		DEBUG_MSG(fmt::format("Interfaces::eraseClientReq: reqCreateAccount_logkey={} set disabled!\n", logkey));
+	}
+
+	REQLOGIN_MAP::iterator liter = reqAccountLogin_requests_.find(logkey);
+	if(liter != reqAccountLogin_requests_.end())
+	{
+		liter->second->enable = false;
+		DEBUG_MSG(fmt::format("Interfaces::eraseClientReq: reqAccountLogin_logkey={} set disabled!\n", logkey));
+	}
+
+	unlockthread();
+}
+
+//-------------------------------------------------------------------------------------
+
+}
---
+++ kbengine-master/kbe/src/server/tools/interfaces/interfaces_tasks.cpp
@@ -0,0 +1,532 @@
+/*
+This source file is part of KBEngine
+For the latest info, see http://www.kbengine.org/
+
+Copyright (c) 2008-2016 KBEngine.
+
+KBEngine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+KBEngine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with KBEngine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "orders.h"
+#include "interfaces.h"
+#include "interfaces_tasks.h"
+#include "network/common.h"
+#include "network/tcp_packet.h"
+#include "network/message_handler.h"
+#include "thread/threadpool.h"
+#include "server/components.h"
+#include "server/serverconfig.h"
+#include "openssl/md5.h"
+
+#include "baseapp/baseapp_interface.h"
+#include "cellapp/cellapp_interface.h"
+#include "baseappmgr/baseappmgr_interface.h"
+#include "cellappmgr/cellappmgr_interface.h"
+#include "loginapp/loginapp_interface.h"
+#include "dbmgr/dbmgr_interface.h"
+
+#if KBE_PLATFORM == PLATFORM_WIN32
+#ifdef _DEBUG
+#pragma comment(lib, "libeay32_d.lib")
+#pragma comment(lib, "ssleay32_d.lib")
+#else
+#pragma comment(lib, "libeay32.lib")
+#pragma comment(lib, "ssleay32.lib")
+#endif
+#endif
+
+
+namespace KBEngine{
+
+//-------------------------------------------------------------------------------------
+InterfacesTask::InterfacesTask()
+{
+}
+
+//-------------------------------------------------------------------------------------
+InterfacesTask::~InterfacesTask()
+{
+}
+
+//-------------------------------------------------------------------------------------
+thread::TPTask::TPTaskState InterfacesTask::presentMainThread()
+{
+	return thread::TPTask::TPTASK_STATE_COMPLETED; 
+}
+
+//-------------------------------------------------------------------------------------
+CreateAccountTask::CreateAccountTask():
+InterfacesTask()
+{
+}
+
+//-------------------------------------------------------------------------------------
+CreateAccountTask::~CreateAccountTask()
+{
+}
+
+//-------------------------------------------------------------------------------------
+void CreateAccountTask::removeLog()
+{
+	Interfaces::getSingleton().lockthread();
+	Interfaces::getSingleton().reqCreateAccount_requests().erase(commitName);
+	Interfaces::getSingleton().unlockthread();
+}
+
+//-------------------------------------------------------------------------------------
+bool CreateAccountTask::process()
+{
+	retcode = SERVER_ERR_OP_FAILED;
+
+	if(!enable)
+	{
+		return false;
+	}
+
+	// 如果没有设置第三方服务地址则我们默认为成功
+	if(strlen(serviceAddr()) == 0)
+	{
+		retcode = SERVER_SUCCESS;
+		getDatas = postDatas;
+		return false;
+	}
+
+	Network::EndPoint endpoint;
+	endpoint.socket(SOCK_STREAM);
+
+	if (!endpoint.good())
+	{
+		ERROR_MSG("InterfacesTask::process: couldn't create a socket\n");
+		return false;
+	}
+
+	if(postDatas.size() == 0)
+	{
+		ERROR_MSG(fmt::format("InterfacesTask::process: {} postData is NULL.\n", commitName));
+		return false;
+	}
+
+	u_int32_t addr;
+	KBEngine::Network::Address::string2ip(serviceAddr(), addr);
+
+	if(endpoint.connect(htons(servicePort()), addr) == -1)
+	{
+		ERROR_MSG(fmt::format("InterfacesTask::process: connect billingserver({}:{}) is error({})!\n", 
+			serviceAddr(), servicePort(), kbe_strerror()));
+
+		endpoint.close();
+		return false;
+	}
+
+	endpoint.setnonblocking(true);
+	endpoint.setnodelay(true);
+
+	Network::Bundle::SmartPoolObjectPtr bundle = Network::Bundle::createSmartPoolObj();
+	(*(*bundle)).append(postDatas.data(), postDatas.size());
+	endpoint.send(bundle->get());
+
+	Network::TCPPacket packet;
+	packet.resize(1024);
+
+	fd_set	frds;
+	struct timeval tv = { 0, 5000000 }; // 5000ms
+
+	FD_ZERO( &frds );
+	FD_SET((int)endpoint, &frds);
+	int selgot = select(endpoint+1, &frds, NULL, NULL, &tv);
+	if(selgot <= 0)
+	{
+		ERROR_MSG(fmt::format("InterfacesTask::process: {} send({}).\n", commitName, postDatas));
+		ERROR_MSG(fmt::format("InterfacesTask::process: {} recv is error({}).\n", commitName, KBEngine::kbe_strerror()));
+		endpoint.close();
+		return false;
+	}
+
+	int len = endpoint.recv(packet.data(), 1024);
+
+	if(len <= 0)
+	{
+		ERROR_MSG(fmt::format("InterfacesTask::process: {} recv is size<= 0.\n===>postdatas={}\n", commitName, postDatas));
+		endpoint.close();
+		return false;
+	}
+
+	packet.wpos(len);
+
+	getDatas.assign((const char *)(packet.data() + packet.rpos()), packet.length());
+
+	try
+	{
+		std::string::size_type fi = getDatas.find("\r\n\r\n");
+		if(fi != std::string::npos)
+		{
+			fi += 4;
+			MemoryStream s;
+			s.append(getDatas.data() + fi, getDatas.size() - fi);
+
+			while(s.length() > 0)
+			{
+				int32 type, len;
+				s >> type >> len;
+				EndianConvertReverse<int32>(type);
+				EndianConvertReverse<int32>(len);
+				
+				int32 error = 0;
+
+				switch(type)
+				{
+				case 1:
+					s >> error;
+					EndianConvertReverse<int32>(error);
+
+					if(error != 0)
+					{
+						retcode = SERVER_ERR_OP_FAILED;
+						endpoint.close();
+						
+						std::string err;
+						if(s.length() >= (sizeof(int32) * 2))
+						{
+							s >> type >> len;
+							
+							if(len > 0 && len < 1024)
+							{
+								char* buf = new char[len + 1];
+								memcpy(buf, s.data() + s.rpos(), len);
+								buf[len] = 0;
+								err = buf;
+								delete[] buf;
+							}
+
+						}
+
+						DEBUG_MSG(fmt::format("InterfacesTask::process: ({})op is failed! err={}\n<==send({})\n==>recv({}).\n", 
+							commitName, err, postDatas, getDatas));
+						
+						return false;
+					}
+					else
+					{
+						retcode = SERVER_SUCCESS;
+					}
+
+					break;
+				case 2:
+					{
+						s.read_skip(len);
+					}
+					break;
+				case 3:
+					{
+						char* buf = new char[len + 1];
+						memcpy(buf, s.data() + s.rpos(), len);
+						buf[len] = 0;
+						accountName = buf;
+						delete[] buf;
+
+						s.read_skip(len);
+					}
+					break;
+				default:
+					break;
+				};
+			}
+		}
+	}
+	catch(...)
+	{
+		retcode = SERVER_ERR_OP_FAILED;
+		ERROR_MSG(fmt::format("InterfacesTask::process: {} recv is error.\n===>postdatas={}\n===>recv={}\n", 
+			commitName, postDatas, getDatas));
+	}
+
+	endpoint.close();
+	enable = true;
+	return false;
+}
+
+//-------------------------------------------------------------------------------------
+thread::TPTask::TPTaskState CreateAccountTask::presentMainThread()
+{
+	if(!enable)
+	{
+		removeLog();
+		DEBUG_MSG(fmt::format("CreateAccountTask::presentMainThread: commitName={} is disable!\n", commitName));
+		return thread::TPTask::TPTASK_STATE_COMPLETED; 
+	}
+
+	Network::Bundle* pBundle = Network::Bundle::ObjPool().createObject();
+
+	(*pBundle).newMessage(DbmgrInterface::onCreateAccountCBFromInterfaces);
+	(*pBundle) << baseappID << commitName << accountName << password << retcode;
+
+	(*pBundle).appendBlob(postDatas);
+	(*pBundle).appendBlob(getDatas);
+
+	Network::Channel* pChannel = Interfaces::getSingleton().networkInterface().findChannel(address);
+
+	if(pChannel)
+	{
+		pChannel->send(pBundle);
+	}
+	else
+	{
+		ERROR_MSG(fmt::format("InterfacesTask::presentMainThread: not found channel. commitName={}\n", commitName));
+		Network::Bundle::ObjPool().reclaimObject(pBundle);
+	}
+
+	removeLog();
+	return thread::TPTask::TPTASK_STATE_COMPLETED; 
+}
+
+//-------------------------------------------------------------------------------------
+LoginAccountTask::LoginAccountTask():
+CreateAccountTask()
+{
+}
+
+//-------------------------------------------------------------------------------------
+LoginAccountTask::~LoginAccountTask()
+{
+}
+
+//-------------------------------------------------------------------------------------
+void LoginAccountTask::removeLog()
+{
+	Interfaces::getSingleton().lockthread();
+	Interfaces::getSingleton().reqAccountLogin_requests().erase(commitName);
+	Interfaces::getSingleton().unlockthread();
+}
+
+//-------------------------------------------------------------------------------------
+thread::TPTask::TPTaskState LoginAccountTask::presentMainThread()
+{
+	if(!enable)
+	{
+		removeLog();
+		DEBUG_MSG(fmt::format("LoginAccountTask::presentMainThread: commitName={} is disable!\n", commitName));
+		return thread::TPTask::TPTASK_STATE_COMPLETED; 
+	}
+
+	Network::Bundle* pBundle = Network::Bundle::ObjPool().createObject();
+	
+	if(retcode == SERVER_ERR_OP_FAILED)
+	{
+		if(accountName.size() == 0)
+			accountName = commitName;
+	}
+
+	(*pBundle).newMessage(DbmgrInterface::onLoginAccountCBBFromInterfaces);
+	(*pBundle) << baseappID << commitName << accountName << password << retcode;
+
+	(*pBundle).appendBlob(postDatas);
+	(*pBundle).appendBlob(getDatas);
+
+	Network::Channel* pChannel = Interfaces::getSingleton().networkInterface().findChannel(address);
+
+	if(pChannel)
+	{
+		pChannel->send(pBundle);
+	}
+	else
+	{
+		ERROR_MSG(fmt::format("InterfacesTask::presentMainThread: not found channel. commitName={}\n", commitName));
+		Network::Bundle::ObjPool().reclaimObject(pBundle);
+	}
+
+	removeLog();
+	return thread::TPTask::TPTASK_STATE_COMPLETED; 
+}
+
+//-------------------------------------------------------------------------------------
+ChargeTask::ChargeTask():
+InterfacesTask(),
+pOrders(NULL),
+retcode(SERVER_ERR_OP_FAILED)
+{
+}
+
+//-------------------------------------------------------------------------------------
+ChargeTask::~ChargeTask()
+{
+}
+
+//-------------------------------------------------------------------------------------
+bool ChargeTask::process()
+{
+	if(!Interfaces::getSingleton().hasOrders(orders.ordersID))
+	{
+		WARNING_MSG(fmt::format("ChargeTask::process: not found ordersID({}), exit threadProcess.\n",
+			orders.ordersID));
+		return false;
+	}
+	
+	KBE_ASSERT(pOrders != NULL);
+
+	pOrders->state = Orders::STATE_FAILED;
+	orders.state = pOrders->state;
+	orders.postDatas = pOrders->postDatas;
+
+	// 如果是不需要请求的直接返回成功
+	if(pOrders->postDatas.size() == 0)
+	{
+		retcode = SERVER_SUCCESS;
+		return false;
+	}
+
+	// 如果没有设置第三方服务地址则我们默认为成功
+	if(strlen(serviceAddr()) == 0)
+	{
+		pOrders->state = Orders::STATE_SUCCESS;
+		pOrders->getDatas = pOrders->postDatas;
+		orders.state = pOrders->state;
+		orders.getDatas = pOrders->getDatas;
+		retcode = SERVER_SUCCESS;
+		return false;
+	}
+
+	Network::EndPoint endpoint;
+	endpoint.socket(SOCK_STREAM);
+
+	if (!endpoint.good())
+	{
+		ERROR_MSG("ChargeTask::process: couldn't create a socket\n");
+		pOrders->getDatas = "couldn't create a socket!";
+		orders.getDatas = pOrders->getDatas;
+		return false;
+	}
+
+	if(pOrders->postDatas.size() == 0)
+	{
+		ERROR_MSG("ChargeTask::process: postData is NULL.\n");
+		pOrders->getDatas = "postDatas is error!";
+		orders.getDatas = pOrders->getDatas;
+		return false;
+	}
+
+	u_int32_t addr;
+	KBEngine::Network::Address::string2ip(serviceAddr(), addr);
+
+	if(endpoint.connect(htons(servicePort()), addr) == -1)
+	{
+		ERROR_MSG(fmt::format("ChargeTask::process: connect billing server is error({})!\n", 
+			kbe_strerror()));
+
+		pOrders->getDatas = "connect is error!";
+		orders.getDatas = pOrders->getDatas;
+		return false;
+	}
+
+	endpoint.setnonblocking(true);
+	endpoint.setnodelay(true);
+
+	Network::Bundle::SmartPoolObjectPtr bundle = Network::Bundle::createSmartPoolObj();
+	endpoint.send(bundle->get());
+
+	Network::TCPPacket packet;
+	packet.resize(1024);
+
+	fd_set	frds;
+	struct timeval tv = { 0, 500000 }; // 500ms
+
+	FD_ZERO( &frds );
+	FD_SET((int)endpoint, &frds);
+	int selgot = select(endpoint+1, &frds, NULL, NULL, &tv);
+	if(selgot <= 0)
+	{
+		ERROR_MSG(fmt::format("InterfacesTask::process: recv is error({}).\n", KBEngine::kbe_strerror()));
+		pOrders->getDatas = "recv is error!";
+		orders.getDatas = pOrders->getDatas;
+		return false;
+	}
+	
+	int len = endpoint.recv(packet.data(), 1024);
+
+	if(len <= 0)
+	{
+		ERROR_MSG(fmt::format("InterfacesTask::process: recv is size<= 0.\n===>postdatas={}\n", pOrders->postDatas));
+		pOrders->getDatas = "recv is error!";
+		orders.getDatas = pOrders->getDatas;
+		return false;
+	}
+
+	packet.wpos(len);
+
+	pOrders->getDatas.assign((const char *)(packet.data() + packet.rpos()), packet.length());
+	orders.getDatas = pOrders->getDatas;
+
+	std::string::size_type fi = pOrders->getDatas.find("retcode:1");
+
+	if(fi != std::string::npos)
+		retcode = SERVER_SUCCESS;
+
+	endpoint.close();
+
+	INFO_MSG(fmt::format("ChargeTask::process: orders={}, commit={}\n==>postdatas={}\n", 
+		pOrders->ordersID, retcode, pOrders->getDatas));
+
+	return false;
+}
+
+//-------------------------------------------------------------------------------------
+thread::TPTask::TPTaskState ChargeTask::presentMainThread()
+{
+	// 如果成功使用异步接收处理
+	if(retcode != SERVER_SUCCESS)
+	{
+		if(!Interfaces::getSingleton().hasOrders(orders.ordersID))
+		{
+			WARNING_MSG(fmt::format("ChargeTask::presentMainThread: not found ordersID({}), exit thread!\n",
+				orders.ordersID));
+
+			return thread::TPTask::TPTASK_STATE_COMPLETED;
+		}
+	
+		Network::Bundle* pBundle = Network::Bundle::ObjPool().createObject();
+
+		(*pBundle).newMessage(DbmgrInterface::onChargeCB);
+		(*pBundle) << orders.baseappID << orders.ordersID << orders.dbid;
+		(*pBundle).appendBlob(orders.getDatas);
+		(*pBundle) << orders.cbid;
+		(*pBundle) << retcode;
+
+		Network::Channel* pChannel = Interfaces::getSingleton().networkInterface().findChannel(orders.address);
+
+		if(pChannel)
+		{
+			WARNING_MSG(fmt::format("ChargeTask::presentMainThread: orders={} commit is failed!\n", 
+				pOrders->ordersID));
+
+			pChannel->send(pBundle);
+		}
+		else
+		{
+			ERROR_MSG(fmt::format("ChargeTask::presentMainThread: not found channel. orders={}\n", 
+				orders.ordersID));
+
+			Network::Bundle::ObjPool().reclaimObject(pBundle);
+		}
+
+		Interfaces::getSingleton().lockthread();
+		Interfaces::ORDERS& ordersmap = Interfaces::getSingleton().orders();
+		Interfaces::ORDERS::iterator iter = ordersmap.find(orders.ordersID);
+		if(iter != ordersmap.end())ordersmap.erase(iter);
+		Interfaces::getSingleton().unlockthread();
+	}
+
+	return thread::TPTask::TPTASK_STATE_COMPLETED; 
+}
+
+//-------------------------------------------------------------------------------------
+}
